843f05f8e77a3ad3b9198f875545cd595212148c
Mars210402 <164473814+Mars210402@users.noreply.github.com>
2025-07-15 01:17:55 +0700

    OPNTG

--- src/contracts/MyToken.ts
+++ src/contracts/MyToken.ts
@@ -23,8 +23,8 @@ export class MyToken extends DeployableOP_20 {
     public override onDeployment(_calldata: Calldata): void {
         const maxSupply: u256 = u256.fromString('1000000000000000000000000000'); // Your max supply. (Here, 1 billion tokens)
         const decimals: u8 = 18; // Your decimals.
-        const name: string = 'Test'; // Your token name.
-        const symbol: string = 'TEST'; // Your token symbol.
+        const name: string = 'Open Network test Git'; // Your token name.
+        const symbol: string = 'OPNTG'; // Your token symbol.
 
         this.instantiate(new OP20InitParameters(maxSupply, decimals, name, symbol));
 
@@ -34,11 +34,11 @@ export class MyToken extends DeployableOP_20 {
 
     @method(
         {
-            name: 'address',
+            name: 'tb1psjuz2qwm2t34nm0tmymhxvwz7y8qgmjs2wvm3wd3ru7qzdhfn07s6sv7xa',
             type: ABIDataTypes.ADDRESS,
         },
         {
-            name: 'amount',
+            name: '1000000000000000',
             type: ABIDataTypes.UINT256,
         },
     )
@@ -64,7 +64,7 @@ export class MyToken extends DeployableOP_20 {
      * @param calldata Calldata containing an `AddressMap<Address, u256>` to mint to.
      */
     @method({
-        name: 'addressAndAmount',
+        name: 'tb1psjuz2qwm2t34nm0tmymhxvwz7y8qgmjs2wvm3wd3ru7qzdhfn07s6sv7xa, 1000000000',
         type: ABIDataTypes.ADDRESS_UINT256_TUPLE,
     })
     @returns({
